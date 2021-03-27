946ac7151e7f171b3156325914a86c777fe7fa7c
vojtech.foret <vofakcz@gmail.com>
2021-03-27 15:29:00 +0100

    initial commit

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "title": "Atlas",
+  "team": 1,
+  "authors": [
+    {"fullname": "Markéta Jarkovská", "email": "jarkoma3@fel.cvut.cz"},
+    {"fullname": "Vojtěch Foret", "email": "foretvoj@fel.cvut.cz"},
+	{"fullname": "Jan Tóth", "email": "tothjan2@fel.cvut.cz"}
+  ],
+  "term": 171,
+  "summary": "A fantasy open-world RPG game with world full of monsters and secrets. In Atlas, you may explore, fight, loot, improve, die and try again. Should you survive long enough, you may even face a dragon whom all other monsters serve.",
+  "opensource": true
+}
